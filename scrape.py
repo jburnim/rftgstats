6d24d0e938dd94800a5a45183f1002c9548c70de
Rob Renaud <rrenaud@gmail.com>
2009-08-21 12:34:08 +0000

    updated scrape to 16000
    git-svn-id: http://rftgstats.googlecode.com/svn/trunk/genie_analysis@37 67826c26-df5b-11dd-9d8c-a70aa0a93a20

--- scrape.py
+++ scrape.py
@@ -11,7 +11,7 @@ def GameComplete(file_name):
                                            MockInfo()) != None
 
 if __name__ == '__main__':
-    for i in range(13000, 14000):
+    for i in range(14000, 16000):
         try:
             url_name = 'game.htm?gid=%d' % i
             url = 'http://genie.game-host.org/' + url_name
